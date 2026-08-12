f3c3bafcb702d21864eb630f9420b2c75f0935f2
Soumyadip Das <soumyadipdas2679@gmail.com>
2026-08-12 23:10:44 +0530

    refactor(client): add modular auth API service

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,13 +1,11 @@
 import API from "./axios";
 
-// Register
-export const registerUser = async (userData) => {
-  const response = await API.post("/auth/register", userData);
-  return response.data;
+export const loginApi = async (credentials) => {
+  const { data } = await API.post("/auth/login", credentials);
+  return data;
 };
 
-// Login
-export const loginUser = async (userData) => {
-  const response = await API.post("/auth/login", userData);
-  return response.data;
+export const registerApi = async (userData) => {
+  const { data } = await API.post("/auth/register", userData);
+  return data;
 };
